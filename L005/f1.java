f8c50527ac3ff0fb7bb398a6e13bf1c40c0ef9a1
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-08-08 20:35:02 +0530

    Minor changes,format document

--- L005/f1.java
+++ L005/f1.java
@@ -1,49 +1,48 @@
 import java.io.*;
 import java.util.*;
+
 class Main {
-  public static void main(String[] args) throws NumberFormatException, IOException {
-    MyLinkedList list = new MyLinkedList();
-    list.add(10);
-    list.add(20);
-    list.add(30);
-    list.add(40);
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        MyLinkedList list = new MyLinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
 
-    for(int val: list){
-      System.out.print(val + " -> ");
-    }
-    System.out.println(".");
-  }
-
-  static class MyLinkedList {
-    static class Node {
-      int data;
-      Node next;
+        // for (int val : list) {
+        //     System.out.print(val + " -> ");
+        // }
+        // System.out.println(".");
     }
 
-    Node head;
-    Node tail;
-    int size;
+    static class MyLinkedList {
+        static class Node {
+            int data;
+            Node next;
+        }
 
-    void addLast(int data){
-      Node node = new Node();
-      node.data = data;
-      node.next = null;
+        Node head;
+        Node tail;
+        int size;
 
-      if(size == 0){
-        head = tail = node;
-      } else {
-        tail.next = node;
-        tail = node;
-      }
+        void addLast(int data) {
+            Node node = new Node();
+            node.data = data;
+            node.next = null;
 
-      size++;
+            if (size == 0) {
+                head = tail = node;
+            } else {
+                tail.next = node;
+                tail = node;
+            }
+
+            size++;
+        }
+
+        int size() {
+            return size;
+        }
     }
-
-    int size(){
-      return size;
-    }
-  }
-
- 
 
 }
