c12929fc0128b18408968b3ddb5f8615bddbad71
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-08-13 14:00:44 +0530

    Previous repo deleted, Eclipse installed and learn't how to override toString function to print the correct frequency of created data structure Car

--- L006/src/PracticeOnObject/Car.java
+++ L006/src/PracticeOnObject/Car.java
@@ -5,7 +5,7 @@ public class Car {
 	int price;
 	int speed;
 	
-	public Car(String name, int price, int speed){
+	Car(String name, int price, int speed){
 		this.name = name;
 		this.price = price;
 		this.speed = speed;
--- L006/src/PracticeOnObject/DriverforToString.java
+++ L006/src/PracticeOnObject/DriverforToString.java
@@ -1,13 +1,17 @@
-import PracticeOnObject.Car;
-import java.util.*;
+package PracticeOnObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class DriverforToString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Car c = new Car("A", 100, 200);
-		// Car c1 = null;
-		System.out.println(c);
+		Car c1 = null;
+		System.out.println(c1);
 		
 		HashMap<String, Integer> map = new HashMap<>();
 		map.put("India", 140);
